7e8efd962ab4e2da496174aff978ee5e8194ca47
miguelangelo78 <santosmiguel25@gmail.com>
2014-09-18 01:31:11 +0100

    Compile 6.0 - Command line arguments added

--- QEPU_Simulator/QEPU_Simulator/QEPU_Simulator.cpp
+++ QEPU_Simulator/QEPU_Simulator/QEPU_Simulator.cpp
@@ -1,7 +1,13 @@
 #include "stdafx.h"
 #include "qepu.h"
-int _tmain(int argc, _TCHAR* argv[]){
-	QEPU q("C:\\Users\\Miguel\\Desktop\\test.bin");
+#include <string>
+int main(int argc, char** argv){
+	if (argc != 2){ std::cerr << "Usage: "<<argv[0]<<" C:\\...\\filename.bin. Exiting..."; std::cin.get(); exit(-1); }
+	
+	std::string binaryfile_path=argv[1];
+	std::string to_replace = ".qep";
+	binaryfile_path.replace(binaryfile_path.find(to_replace), std::string(to_replace).length(),".bin");
+	QEPU q((char*) binaryfile_path.c_str());
 	q.run();
 	return 0;
 }
