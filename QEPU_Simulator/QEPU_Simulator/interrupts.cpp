0beadfcb30920cc71af2541a8bc3b9d108c21979
miguelangelo78 <santosmiguel25@gmail.com>
2014-09-27 21:24:49 +0100

    Compile 11.0 - More interrupts added
    GOTOXY, GOTOXY_RESET, WRITEAT and WRITESTRAT functions added

--- QEPU_Simulator/QEPU_Simulator/interrupts.cpp
+++ QEPU_Simulator/QEPU_Simulator/interrupts.cpp
@@ -2,20 +2,22 @@
 #include "qepu.h"
 void QEPU::interrupt_cpu(int interrupt_type){
 	interrupt_signal = false;
+	bool interrupt_init = false;
 	//NEEDS RESERVED REGISTERS USED FOR ADDRESS POINTING -> ADDRESS SHOULD POINT TO A REGISTER ADDRESS,NOT SRAM
 	//NEEDS RESERVED REGISTERS USED FOR DATA SAVING -> SHOULD SAVE ON A REGISTER,NOT SRAM
 	int address_pointer_offset = 0;
-	int data_out_registerpointer = qmem.fetch_register(INT_RESERVED_ADDRESSPOINTER);
+	//SPECIAL ADDRESSESS:
+	int data_buffer_pointer = qmem.fetch_register(INT_RESERVED_ADDRESSPOINTER);
 	int data_in_registerpointer = qmem.fetch_register(INT_RESERVED_ADDRESSDATASTORE);
 	while (!interrupt_signal){
 		switch (interrupt_type){
 			case INT_UART_OUT_CHAR:
-				serial.write(sram.read(data_out_registerpointer));
+				serial.write(sram.read(data_buffer_pointer));
 				interrupt_done();
 				break;
 			case INT_UART_OUT_BUFFER:
 				{
-					int data_read = sram.read(data_out_registerpointer + address_pointer_offset);
+					int data_read = sram.read(data_buffer_pointer + address_pointer_offset);
 					if (data_read == STRING_TERMINATOR) interrupt_done();
 					else{
 						serial.write(data_read);
@@ -68,7 +70,35 @@ void QEPU::interrupt_cpu(int interrupt_type){
 					}
 				}
 				break;
+			case INT_UART_OUT_CHARAT:
+				serial.writeat(sram.read(data_buffer_pointer), qmem.fetch_register(INT_REGDATA_BUFFER), qmem.fetch_register(INT_REGDATA_BUFFER + 1));
+				interrupt_done();
+				break;
+			case INT_UART_OUT_BUFFAT:
+				{
+					if (!interrupt_init) serial.gotoxy(qmem.fetch_register(INT_REGDATA_BUFFER), qmem.fetch_register(INT_REGDATA_BUFFER + 1));
+					int data_read = sram.read(data_buffer_pointer + address_pointer_offset);
+					if (data_read == STRING_TERMINATOR){interrupt_done();serial.gotoxy_reset();}
+					else{
+						serial.write(data_read); 
+						address_pointer_offset++;
+					}
+				}
+				break;
+			case INT_UART_GOTOXY:
+				serial.gotoxy(qmem.fetch_register(INT_REGDATA_BUFFER), qmem.fetch_register(INT_REGDATA_BUFFER + 1));
+				interrupt_done();
+				break;			
+			case INT_UART_GOTOXY_RESET:
+				serial.gotoxy_reset();
+				interrupt_done();
+				break;
+			case INT_UART_CLRSCR:
+				serial.clrscr();
+				interrupt_done();
+				break;
 		}
+		interrupt_init = true;
 	}
 	interrupt_signal = false;
 }
--- QEPU_Simulator/QEPU_Simulator/uart.h
+++ QEPU_Simulator/QEPU_Simulator/uart.h
@@ -5,13 +5,27 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
+
 class Serial{
-public:
-	Serial();
-	char read();
-	void write(char data);
-	void writestr(char * data);
-	void writeln(char data);
-	void writestrln(char * data);
+	public:
+		Serial();
+		char read();
+		void write(char data);
+		void writestr(char * data);
+		void writeln(char data);
+		void writestrln(char * data);
+		void writeat(char data, int x, int y);
+		void writestrat(char * data, int x, int y);
+		void gotoxy_reset();
+		void gotoxy(int x, int y);
+		void clrscr();
+	private:
+		int last_cursor_x;
+		int last_cursor_y;
+		int console_times_cleared;
+		bool is_lastcurs_def;
+		//EXCLUSIVE FOR THE HARDWARE SIMULATOR:
+		int get_console_width();
+		int get_console_height();
 };
 #endif
